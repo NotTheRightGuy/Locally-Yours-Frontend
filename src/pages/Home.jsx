c311dd69d826d94b9a07be4d25e34b25e450dd89
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-07-29 19:22:22 +0530

    Added TypeWriter Effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,39 @@
 import NavBar from "../components/Navbar";
 import LandingNews from "../components/LandingNews";
+import { useRef, useEffect } from "react";
+import TypeWriter from "typewriter-effect/dist/core";
+
 function Home() {
+    const typewriter = useRef(null);
+    useEffect(() => {
+        const typewriterInstance = new TypeWriter(typewriter.current, {
+            delay: 50,
+            loop: true,
+            deleteSpeed: 10,
+        });
+
+        typewriterInstance
+            .pauseFor(200)
+            .typeString("SIMPLIFYING THE NEWS FOR YOU")
+            .pauseFor(400)
+            .deleteChars(28)
+            .typeString("EASING THE WAY YOU READ NEWS")
+            .pauseFor(400)
+            .deleteChars(32)
+            .typeString("HELPING YOU STAY UPDATED")
+            .pauseFor(400)
+            .deleteChars(24)
+            .start();
+    }, []);
     return (
         <section className="landing-page">
             <NavBar />
             <hr />
             <div className="flex mt-10 px-10">
-                <div className="w-[50vw] font-bebas font-bold text-6xl tracking-wide scale-y-125 translate-y-2">
+                <div
+                    ref={typewriter}
+                    className="w-[50vw] font-bebas font-bold text-6xl tracking-wide scale-y-125 translate-y-2"
+                >
                     SIMPLIFYING THE NEWS FOR YOU
                 </div>
                 <div className="w-[50vw] font-poppins font-semibold">
