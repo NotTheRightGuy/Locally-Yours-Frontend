519450a335afc2e95c3f716099923ea3abb39c05
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-07-30 10:55:02 +0530

    Added preloader

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import CategorySelection from "./pages/CategorySelection";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 import ChangePinCode from "./pages/UpDatePincode";
+import PreLoader from "./components/PreLoader";
 
 import { ClerkProvider, SignOutButton } from "@clerk/clerk-react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -15,7 +16,15 @@ function App() {
         <ClerkProvider publishableKey="pk_test_ZXZvbHZpbmcta2FuZ2Fyb28tMzcuY2xlcmsuYWNjb3VudHMuZGV2JA">
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <PreLoader />
+                                <Home />
+                            </>
+                        }
+                    />
                     <Route path="/signin" element={<SignInPage />} />
                     <Route path="/signup" element={<SignUpPage />} />
                     <Route
---
+++ src/components/PreLoader.jsx
@@ -0,0 +1,50 @@
+import "../stylesheets/Preloader.css";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
+function PreLoader() {
+    const preloader = useRef(null);
+    useEffect(() => {
+        gsap.fromTo(
+            ".future",
+            {
+                duration: 1.5,
+                x: 200,
+                ease: "Expo.easeInOut",
+            },
+            {
+                duration: 1.5,
+                x: 0,
+                ease: "Expo.easeInOut",
+            }
+        );
+        gsap.to(".preloader__container-top", {
+            duration: 1,
+            delay: 1.25,
+            top: "-100%",
+            ease: "Expo.easeInOut",
+        });
+        gsap.to(".preloader__container-bottom", {
+            duration: 1,
+            delay: 1.25,
+            bottom: "-100%",
+            ease: "Expo.easeInOut",
+        });
+        gsap.to(".preloader", {
+            duration: 1,
+            delay: 1.25,
+            display: "none",
+            ease: "Expo.easeInOut",
+        });
+    });
+    return (
+        <section className="preloader" ref={preloader}>
+            <div className="preloader__container-top">
+                SIMPIFYING THE <span className="future">FUTURE</span>
+            </div>
+            <div className="preloader__container-bottom">OF READING NEWS</div>
+        </section>
+    );
+}
+
+export default PreLoader;
